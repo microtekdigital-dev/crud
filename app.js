8a5532a6de84b461e75af426811dda6f7d84e8ef
microtekdigital-dev <microtekdigital@gmail.com>
2026-01-11 11:11:24 -0300

    Change API endpoint to production URL

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const api = "http://127.0.0.1:8000";
+const api = "https://crud-vfxn.onrender.com";
 
 // Agregar producto
 document.getElementById("agregar").addEventListener("click", async () => {
